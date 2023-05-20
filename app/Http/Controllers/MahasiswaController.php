792b069a8ee1da1328b6546665269f4b9786b320
justhussh <azzam17102003@gmail.com>
2023-05-20 14:33:13 +0700

    Praktikum 5

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -102,6 +102,7 @@ class MahasiswaController extends Controller
      */
     public function destroy(Mahasiswa $mahasiswa)
     {
-        //
+        $mahasiswa->delete();
+        return response()->noContent();
     }
 }
